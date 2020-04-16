9150b0351df925cb241c64f726cbb5273ad0354f
trf <trf.studios246@gmail.com>
2020-04-16 15:02:28 -0400

    node

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const sql = require('mysql');
 const app = express();
 var path = require('path');
 
 const port = process.env.PORT || 3000; 
+
+
+var con = sql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: '',
+  });
+
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query("CREATE DATABASE hospitalDB", function (err, result) {
+        // if (err) throw err;
+        console.log("Database created");
+      });
+
+      
+  });
+
+
+ 
+
 
 console.log(port);
 app.use('/css', express.static('./css'))
@@ -18,11 +42,18 @@ app.get('/index.html',function(req,res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 } );
 
+app.get('/home',function(req,res) {
+    res.sendFile(path.join(__dirname + '/home.html'));
+} );
 app.get('/login.html',function(req,res) {
     res.sendFile(path.join(__dirname + '/login.html'));
 } );
 
 app.get('/faq.html',function(req,res) {
+    res.sendFile(path.join(__dirname + '/faq.html'));
+} );
+
+app.get('/faqadmin.html',function(req,res) {
     res.sendFile(path.join(__dirname + '/faq.html'));
 } );
 
