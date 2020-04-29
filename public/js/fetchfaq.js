95886082a4f6c7797f55d3a43b4e0ebc6a97e0ec
geewizz246 <geewizz246@users.noreply.github.com>
2020-04-29 02:22:33 -0400

    For Tabari: I don't know how to get the edit fields to show up

--- public/js/fetchfaq.js
+++ public/js/fetchfaq.js
@@ -2,56 +2,67 @@ const url = '/faq/select';
 const windowUrl = window.location.toString();
 
 fetch(url)
-.then(response => response.json())
-.then(function(data) {
-  console.log(data.faq[0]);
-  for(var i = 0; i < data.faq.length; i++)
-  {
-    let accordion =  document.getElementById('accordion');
-    let col = document.createElement('div').setAttribute('id','col-sm-12');
-    let row = document.createElement('div').setAttribute('id','row');
-    let container = document.createElement('div').setAttribute('id','container');
+  .then(response => response.json())
+    .then(function(data) {
+      console.log(data.faq[0]);
+      for(var i = 0; i < data.faq.length; i++)
+      {
+        let accordion =  document.getElementById('accordion');
+        let col = document.createElement('div').setAttribute('id','col-sm-12');
+        let row = document.createElement('div').setAttribute('id','row');
+        let container = document.createElement('div').setAttribute('id','container');
 
-    if (windowUrl.includes('/faq/guest')) {
-      accordion.innerHTML += 
-        `<div class="panel initial-line">
-            <div class="line-header p-3 mb-3" role="tab" id="line2">
-                <h3 class="line-name">
-                    <a class="collapsed text-dark" role="button" title="" data-toggle="collapse" data-parent="#accordion"
-                        href="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}">
-                        ${data.faq[i]['ques']}
-                    </a>
-                </h3>
-            </div>
-            <div id="collapse${i}" class="panel-collapse collapse" role="tabpanel" aria-labelledby="line2">
-                <div class="panel-body px-3 mb-4">
-                    <p>${data.faq[i]['answer']}
+        if (windowUrl.includes('/faq/guest')) {
+          accordion.innerHTML += 
+            `<div class="panel initial-line">
+                <div class="line-header p-3 mb-3" role="tab" id="line2">
+                    <h3 class="line-name">
+                        <a class="collapsed text-dark" role="button" title="" data-toggle="collapse" data-parent="#accordion"
+                            href="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}">
+                            ${data.faq[i]['ques']}
+                        </a>
+                    </h3>
                 </div>
-            </div>
-        </div>`;
-    } else if (windowUrl.includes('/faq/admin')) {
-      accordion.innerHTML += 
-        `<div class="panel initial-line">
-            <div class="line-header p-3 mb-3" role="tab" id="line2">
-                <h3 class="line-name">
-                    <a class="collapsed text-dark" role="button" title="" data-toggle="collapse" data-parent="#accordion"
-                        href="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}">
-                        ${data.faq[i]['ques']}
-                    </a>
-                </h3>
-            </div>
-            <div id="collapse${i}" class="panel-collapse collapse" role="tabpanel" aria-labelledby="line2">
-                <div class="panel-body px-3 mb-4">
-                    <p>${data.faq[i]['answer']}
-                    <br> ${data.faq_log[i]['action']}: ${data.faq_log[i]['timestamp']}
-                    </p>
-                    <button id="faq-edit=${i}" class="btn btn-link btn-faq-edit" type="button">Edit</button>
+                <div id="collapse${i}" class="panel-collapse collapse" role="tabpanel" aria-labelledby="line2">
+                    <div class="panel-body px-3 mb-4">
+                        <p>${data.faq[i]['answer']}
+                    </div>
                 </div>
-            </div>
-        </div>`;
+            </div>`;
+        } else if (windowUrl.includes('/faq/admin')) {
+          accordion.innerHTML += 
+            `<div class="panel initial-line">
+                <div class="line-header p-3 mb-3" role="tab" id="line2">
+                    <h3 class="line-name">
+                        <a class="collapsed text-dark" role="button" title="" data-toggle="collapse" data-parent="#accordion"
+                            href="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}">
+                            ${data.faq[i]['ques']}
+                        </a>
+                    </h3>
+                </div>
+                <div id="collapse${i}" class="panel-collapse collapse" role="tabpanel" aria-labelledby="line2">
+                    <div class="panel-body px-3 mb-4">
+                        <p>${data.faq[i]['answer']}
+                        <br> ${data.faq_log[i]['action']}: ${data.faq_log[i]['timestamp']}
+                        </p>
+                        <button id="faq-edit=${i}" class="btn btn-link btn-faq-edit" type="button">Edit</button>
+                    </div>
+                </div>
+            </div>`;
+          }
       }
-  }
-})
-.catch(function(error) {
-  console.error(error);
-});
+    })
+    .finally(() => {
+      // On click any edit button for FAQ, show the corresponding edit field
+      document.querySelectorAll('.btn-faq-edit').forEach((btn) => {
+        btn.addEventListener('click', showEditFields);
+      })
+    })
+  .catch(function(error) {
+    console.error(error);
+  });
+
+
+let showEditFields = function(e) {
+
+}
